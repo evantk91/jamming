6ce85e0de7075896937a72e206a1ab9645a88857
etgreer91 <etgreer91@gmail.com>
2020-07-30 16:04:31 -0600

    creates the components used in the application

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TrackList from '../TrackList';
+import './SearchResults.css';
+
+class SearchResults extends Component {
+  render() {
+    return(
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <!-- Add a TrackList component --> */}
+      </div>    
+    )
+  }
+}
+
+export default SearchResults;
